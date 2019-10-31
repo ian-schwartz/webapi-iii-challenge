30c06065833faa494834e7c8fe78ffc8e3cf0b5d
Ian Schwartz <ian.schwartz23@gmail.com>
2019-10-31 12:42:15 -0700

    heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./server');
+require('dotenv').config();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
     console.log(`\n*** Server running on port ${port} ***\n`);
 })
